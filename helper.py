cd524a7c27b74052226cc16f07723406e0f86ac0
mohammad-oghli <mhd.sh.oghli@gmail.com>
2023-10-27 17:16:22 +0400

    add plot_pie function

--- helper.py
+++ helper.py
@@ -1,3 +1,5 @@
+import plotly.graph_objects as plt
+
 
 def validate_webpage(r):
     if 'text/html' in r.headers['content-type']:
@@ -12,20 +14,34 @@ def scrap_links(url, links):
         link_dict['name'] = ' '.join(a.text.split())
         href = a.get('href')
         if href:
-            #print(href)
+            # print(href)
             if href[0] == "#":
                 link_dict['sc_link'] = url + "/#"
-                #sc_links.append(url + "/#")
+                # sc_links.append(url + "/#")
             elif href[0] == "/":
                 link_dict['sc_link'] = url + href
-                #sc_links.append(url + href)
+                # sc_links.append(url + href)
             else:
                 if "http" in href:
                     link_dict['sc_link'] = href
-                    #sc_links.append(href)
+                    # sc_links.append(href)
             if 'sc_link' in link_dict:
                 link_list.append(link_dict)
     return link_list
+
+
+def plot_pie(total_a, total_b):
+    url_health = ['HEALTHY', 'BROKEN']
+    values = [total_a, total_b]
+    fig = plt.Figure(
+        plt.Pie(
+            labels=url_health,
+            values=values,
+            hoverinfo="label+percent",
+            textinfo="percent",
+            marker=dict(colors=['#83c9ff', '#b2182b'])
+        ))
+    return fig
 
 
 def display_sources(src_arr):
